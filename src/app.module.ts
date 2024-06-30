1491e735087be087a093ad10c7f94a07fb3d9ce0
Artsem Belavusau <artsem.belavusau@finastra.com>
2024-06-29 21:07:58 -0400

    generate users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {PricesModule} from "./prices/prices.module";
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PricesModule, AuthModule],
+  imports: [PricesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
