77945ea25a2583d4ea4470a68d55aa09c4952649
Artsem Belavusau <artsem.belavusau@finastra.com>
2024-06-29 21:06:42 -0400

    ---------start commit. generate Auth module
    (cherry picked from commit f817c46d94ec38d7e2875562b6c8cf3c04dbfb4f)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {PricesModule} from "./prices/prices.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PricesModule],
+  imports: [PricesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
