405840bd5121874d4cc2aa081cf577117a69a4c4
Artsem Belavusau <artsem.belavusau@finastra.com>
2024-06-30 00:03:29 -0400

    implement auth with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {UsersModule} from "../users/users.module";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "./constants";
 
+// @Module({
+//   imports: [UsersModule],
+//   controllers: [AuthController],
+//   providers: [AuthService]
+// })
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret, //TODO: use env secret value instead in prod env
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import {JwtService} from "@nestjs/jwt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) {}
+
+    async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+        const user = await this.usersService.findOne(email);
+        if (user?.password !== pass) {
+            throw new UnauthorizedException();
+        }
+        // const { password, ...result } = user;
+        // return result;
+
+        // TODO: Generate a JWT and return it here
+        // instead of the user object
+        const payload = { sub: user.userId, username: user.username };
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        };
+    }
+}
